ffcf69d95ac51d48ff27aa2baacb763dd8451a56
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-11-15 06:09:15 +0500

    Adding some icons, doing half work of responsive for navbar

--- travel.int/src/components/Home/Navbar.js
+++ travel.int/src/components/Home/Navbar.js
@@ -22,6 +22,12 @@ function Navbar() {
           Sign up
         </SignUp>
       </NavButtons>
+      <NavMenu>
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
+      </NavMenu>
+      <MenuClose>
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+      </MenuClose>
     </Container>
   )
 }
@@ -33,6 +39,10 @@ const Container = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 25px 60px;
+
+  @media screen and (max-width: 900px){
+    padding: 25px 40px;
+  }
 `
 
 const NavLogo = styled.div`
@@ -43,6 +53,10 @@ const NavLogo = styled.div`
 const NavList = styled.ul`
   display: flex;
   align-items: center;
+
+  @media screen and (max-width: 768px){
+    display: none;
+  }
 
   > li {
     margin: 0 20px;
@@ -58,13 +72,20 @@ const NavList = styled.ul`
         font-weight: 600;
       }
     }
-    
+    @media screen and (max-width: 900px){
+    margin: 0 10px;
+  }
   }
 `
 
 const NavButtons = styled.div`
   display: flex;
   align-items: center;
+
+  @media screen and (max-width: 768px){
+    display: none;
+  }
+
 `
 
 const LoginIn = styled.a`
@@ -90,5 +111,28 @@ const SignUp = styled.a`
     color: #3075C6;
     background-color: transparent;
 
+  }
+`
+
+const NavMenu = styled.div` 
+  width: 20px;
+  height: 20px;
+
+  path {
+    color: #3075C6;
+  }
+
+  @media screen and (min-width: 768px){
+    display: none;
+  }
+`
+
+const MenuClose = styled.div`
+  width: 22px;
+  height: 22px;
+  display: none;
+
+  @media screen and (min-width: 768px){
+    display: none;
   }
 `
