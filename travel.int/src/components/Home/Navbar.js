5be7db432d6c628e2d4726622d3667cd94c8c93a
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-11-15 20:32:25 +0500

    Adding responsive for navbar

--- travel.int/src/components/Home/Navbar.js
+++ travel.int/src/components/Home/Navbar.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <Container className="container">
       <NavLogo>
         <a href="/"><img src="/images/Home/website__logo.svg" alt="Website Logo - Travel-int"/></a>
       </NavLogo>
-      <NavList>
+      <NavList isOpen={isOpen}>
         <li><a>Home</a></li>
         <li><a>Category</a></li>
         <li><a>About us</a></li>
@@ -22,11 +23,11 @@ function Navbar() {
           Sign up
         </SignUp>
       </NavButtons>
-      <NavMenu>
+      <NavMenu onClick={() => setIsOpen(true)}>
         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
       </NavMenu>
-      <MenuClose>
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+      <MenuClose onClick={() => setIsOpen(false)} isOpen={isOpen}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>
       </MenuClose>
     </Container>
   )
@@ -55,7 +56,24 @@ const NavList = styled.ul`
   align-items: center;
 
   @media screen and (max-width: 768px){
-    display: none;
+    display: ${props => props.isOpen ? "" : "none"};
+    position: fixed;
+    top: -10px;
+    right: -10px;
+    width: 70%;
+    height: 90%;
+    flex-direction: column;
+    box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+    z-index: 10;
+    background-color: white;
+    transition: all 0.5s ease-in-out;
+    padding-top: 100px;
+    
+
+    > li {
+      padding: 20px;
+    }
+    
   }
 
   > li {
@@ -105,8 +123,8 @@ const SignUp = styled.a`
   font-weight: 700;
   cursor: pointer;
   border-radius: 5px;
-  transition: all 0.3s ease;
   border: 2px solid #3075C6;
+  transition: all 0.3s ease;
   &:hover {
     color: #3075C6;
     background-color: transparent;
@@ -120,6 +138,10 @@ const NavMenu = styled.div`
 
   path {
     color: #3075C6;
+
+    &:active {
+    color: black;
+    }
   }
 
   @media screen and (min-width: 768px){
@@ -128,11 +150,25 @@ const NavMenu = styled.div`
 `
 
 const MenuClose = styled.div`
-  width: 22px;
-  height: 22px;
-  display: none;
+  display: ${props => props.isOpen ? "" : "none"};
+  position: absolute;
+  top: 33px;
+  right: 40px;
+  z-index: 10;
 
+  
+
+  > svg {
+    width: 19px;
+    height: 19px;
+    fill: #3075C6;
+
+    &:active {
+    fill: black;
+    }
+  }
   @media screen and (min-width: 768px){
     display: none;
   }
+
 `
