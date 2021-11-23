7dc2c694b0fea0f9a5b52d1c01f82bc2b546ef26
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-11-23 07:46:33 +0500

    Adding some background images for category section and make a responsive for category

--- travel.int/src/components/Features.js
+++ travel.int/src/components/Features.js
@@ -43,24 +43,70 @@ const Container = styled.div`
   position: relative;
   display: flex;
   justify-content: space-between;
-  padding: 100px 150px;
+  padding: 80px 150px;
   align-items: center;
+  background-image: 
+  url("/images/Home/features__background--blue-16.svg"),
+  url("/images/Home/features__background--blue-18.svg"),
+  url("/images/Home/features__background--green-17.svg"),
+  url("/images/Home/features__background--green-24.svg"),
+  url("/images/Home/features__background--blue-27.svg"),
+  url("/images/Home/features__background--red-11.svg"),
+  url("/images/Home/features__background--red-20.svg");
+  background-repeat: no-repeat;
+  background-position: 
+  left calc(50% + 0px) top 80%,
+  left calc(50% + 100px) top 0,
+  left calc(77% + 0px) top 14%,
+  left calc(44% + 0px) top 55%,
+  left calc(92% + 0px) top 60%,
+  left calc(47% + 0px) top 18%,
+  left calc(87% + 0px) top 38%;
+
+  @media screen and (max-width: 924px){
+    padding: 60px 80px;
+  }
+  @media screen and (max-width: 850px){
+    flex-direction: column;
+    justify-content: center;
+  }
+  @media screen and (max-width: 600px){
+    padding: 30px 30px;
+  }
 `
 const FeaturesImage = styled.div`
   width: 47%;
+  @media screen and (max-width: 924px){
+    width: 42%;
+  }
+  @media screen and (max-width: 850px){
+    width: 70%;
+  }
+  @media screen and (max-width: 550px){
+    width: 90%;
+    margin: 0 auto;
+    transform: translateX(20px);
+  }
+  @media screen and (max-width: 470px){
+    width: 100%;
+  }
 `
 const MainImage = styled.img`
   width: 100%;
   max-width: 385px;
+  @media screen and (max-width: 400px){
+    width: 100%;
+    max-width: 280px;
+  }
 `
 const Destinations = styled.div`
   position: absolute;
   display: flex;
-  width: 190px;
+  width: 185px;
   padding: 12px 0 13px 17px;
   background: rgba(255, 255, 255, 50%);
   top: 350px;
-  left: 50px;
+  left: calc(5% - 0px);
   border-top-left-radius: 35px;
   border-bottom-left-radius: 15px;
   border-bottom-right-radius: 100px;
@@ -71,18 +117,42 @@ const Destinations = styled.div`
     width: 30px;
     height: 30px;
     margin-right: 30px;
+    @media screen and (max-width: 550px){
+      margin-right: 15px;
+    }
   }
   span {
     h3 {
       font-size: 24px;
       font-weight: 700;
       color: #313131;
+
+      @media screen and (max-width: 550px){
+      font-size: 20px;
+      }
     }
     h5 {
       font-size: 12px;
       color: #5F5F5F;
       font-weight: 700;
+      @media screen and (max-width: 550px){
+        font-size: 10px;
+      }
     }
+  }
+  @media screen and (max-width: 550px){
+    width: 140px;
+    padding: 5px 0 5px 10px;
+    top: 350px;
+    left: calc(10% - 80px);
+  }
+  @media screen and (max-width: 450px){
+    top: 350px;
+    left: calc(10% - 80px);
+  }
+  @media screen and (max-width: 400px){
+    top: 250px;
+    left: calc(10% - 80px);
   }
 `
 const Customers = styled(Destinations)`
@@ -93,6 +163,32 @@ const Customers = styled(Destinations)`
   border-bottom-right-radius: 15px;
   border-bottom-left-radius: 100px !important;
   padding-left: 30px;
+
+  @media screen and (max-width: 1180px){
+    top: 250px;
+    left: 290px;
+  }
+  @media screen and (max-width: 850px){
+    top: 200px;
+    left: 430px;
+  }
+  @media screen and (max-width: 650px){
+    top: 200px;
+    left: 350px;
+  }
+  @media screen and (max-width: 550px){
+    padding-left: 20px;
+    top: 120px;
+    left: 250px;
+  }
+  @media screen and (max-width: 450px){
+    top: 120px;
+    left: 200px;
+  }
+  @media screen and (max-width: 400px){
+    top: 120px;
+    left: 170px;
+  }
 `
 const Tours = styled(Destinations)`
   top: 530px;
@@ -102,11 +198,47 @@ const Tours = styled(Destinations)`
   border-bottom-right-radius: 35px;
   border-bottom-left-radius: 10px;
   padding-left: 60px;
+  @media screen and (max-width: 1200px){
+    left: 290px;
+  }
+  @media screen and (max-width: 850px){
+    top: 450px;
+    left: 390px;
+  }
+  @media screen and (max-width: 580px){
+    top: 420px;
+    left: 280px;
+  }
+  @media screen and (max-width: 550px){
+    padding-left: 30px;
+    top: 420px;
+    left: 250px;
+  }
+  @media screen and (max-width: 450px){
+    top: 420px;
+    left: 200px;
+  }
+  @media screen and (max-width: 400px){
+    top: 320px;
+    left: 170px;
+  }
 `
 
 // Right Part of Features
 const FeaturesInfo = styled.div`
   width: 47%;
+  @media screen and (max-width: 924px){
+    width: 42%;
+  }
+  @media screen and (max-width: 850px){
+    margin-top: 100px;
+    text-align: center;
+    width: 90%;
+  }
+  @media screen and (max-width: 400px){
+    margin-top: 80px;
+    width: 100%;
+  }
 
   h2 {
     font-size: 42px;
@@ -116,12 +248,20 @@ const FeaturesInfo = styled.div`
     span {
       color: #3075C6;
     }
+    @media screen and (max-width: 400px){
+      font-size: 32px;
+      margin-bottom: 30px;
+    }
   }
   p {
     font-size: 22px;
     font-weight: 600;
     color: #AEAEAE;
     line-height: 33px;
+    @media screen and (max-width: 400px){
+      font-size: 18px;
+      line-height: 28px;
+    }
   }
 `
 
