f4c45930c15638f0249a21682954f0235eb60a7e
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-12-14 22:04:54 +0500

    Adding some router elements and creating home file

--- wehr-dashboard/src/App.js
+++ wehr-dashboard/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Home from './components/Home/Home';
 import SignUp from './components/SignUp/SignUp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-      <SignUp/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/signUp" element={<SignUp/>}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
