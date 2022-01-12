3f8f9646bb084e818042569163ca127a51a9d71c
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-12 21:13:32 +0500

    Finishing Top Side of Footer component

--- online-commerce/src/components/Footer/FooterItems.js
+++ online-commerce/src/components/Footer/FooterItems.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 const BuyData = ["Payment methods", "Order and pick up", "Order with delivery", "Shopping over the phone", "Returns"]
 const HelpData = ["Contact", "Online Help", "Our Commitments", "Give feedback"]
 const ServicesData = ["Transport", "Design service", "Paint an plaster mixing service", "Dimensioning and assemly service", "Return of used equipment", "Additional services"]
-const AboutData = ["About us", "Press Office", "For suppliers", "Regulations", "Privacy Policy", "Cookies", "Cookies"]
+const AboutData = ["About us", "Press Office", "For suppliers", "Regulations", "Privacy Policy", "Cookies", "Personal Data Request"]
 
 function CommonItem({data, heading}){
   return (
@@ -11,7 +11,7 @@ function CommonItem({data, heading}){
       <h3 className="footer__heading">{heading}</h3>
       <ul>
         {data.map((element) => (
-          <li><a>{element}</a></li>
+          <li><a href="#">{element}</a></li>
         ))}
       </ul>
     </>
@@ -25,24 +25,24 @@ function FooterItems() {
         <h3 className="footer__heading">Sign up for news</h3>
         <p>Keep up to date with the latest product launches and news. 
           Find out more about our brands and get special promo codes.</p>
-        <input type="email"/>
+        <input className="footer__email" type="email" placeholder="Your e-mail address"/>
         <button type="submit">Sign up for newsletter</button>
-        <div>
-          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
+        <div className="agreement">
+          <input type="checkbox" id="id" name="name" value="value"/>
           <p>I accept <span>the personal data treatment.</span></p>
         </div>
       </SignUp>
       <Links>
-        <div>
+        <div className="Link">
           <CommonItem data={BuyData} heading="How to buy"/>
         </div>
-        <div>
+        <div className="Link">
           <CommonItem data={HelpData} heading="Help"/>
         </div>
-        <div>
+        <div className="Link">
           <CommonItem data={ServicesData} heading="Services"/>
         </div>
-        <div>
+        <div className="Link">
           <CommonItem data={AboutData} heading="About"/>
         </div>
       </Links>
@@ -57,20 +57,92 @@ const Items = styled.div`
   display: flex;
   align-items: stretch;
   justify-content: space-between;
+  color: #0C0C0C;
 
   .footer__heading {
-    color: #0C0C0C;
     font-size: 19px;
+    margin-bottom: 24px;
   }
 
 `
 const SignUp = styled.div`
   max-width: 300px;
   width: 100%;
+
+  p {
+    line-height: 22px;
+  }
+  .footer__email {
+    border: none;
+    outline: none;
+    border: 1px solid #EAECEE;
+    padding: 12px 16px;
+    width: 100%;
+    border-radius: 8px;
+    margin: 24px 0;
+
+    &::placeholder {
+      color: #737B7D;
+      font-size: 14px;
+      
+    }
+  }
+  button {
+    border: none;
+    background-color: #1071FF;
+    color: white;
+    font-size: 16px;
+    font-weight: 500;
+    width: 100%;
+    padding: 12px 0;
+    border-radius: 30px;
+    cursor: pointer;
+    margin-bottom: 10px;
+    transition: all 0.3s ease;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+  .agreement {
+    display: flex;
+    align-items: center;
+    input {
+      margin-right: 8px;
+    }
+  }
+  p {
+    font-size: 14px;
+    margin-top: 3px;
+    span {
+      color: #1071FF;
+    }
+  }
 `
 const Links = styled.div`
   display: flex;
   align-items: stretch;
   justify-content: space-between;
+
+  .Link {
+    max-width: 210px;
+    margin-left: 60px;
+
+    ul {
+      display: flex;
+      flex-direction: column;
+      color: #0C0C0C;
+      li {
+        margin-bottom: 10px;
+        a {
+          color: #0C0C0C;
+          cursor: pointer;
+          transition: all 0.2s ease;
+          &:hover {
+            color: #1071FF;
+          }
+        }
+      }
+    }
+  }
 `
 
